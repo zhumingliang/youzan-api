aeae3e66fa009eb692b98add853a5085f032dede
mingliangZhu <mlz@email.com>
2017-09-17 17:20:53 +0800

    测试

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,13 +5,15 @@ namespace app\index\controller;
 //use youzanlib\Toekn;
 
 use think\Loader;
-use YouZan\Token;
+
+
+//use YouZan\Token;
 
 class Index
 {
     public function index()
     {
-        Loader::import('YouZan\Token', EXTEND_PATH);
-       $token=new Token();
+        Loader::import('YouZan.Test', EXTEND_PATH);
+       $token=new \Test();
     }
 }
