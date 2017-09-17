813d1cc2a474e4aa2610de37b10b5302fee7fa25
mingliangZhu <mlz@email.com>
2017-09-17 22:28:27 +0800

    测试

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\index\controller;
+namespace api\index\controller;
 
 use think\Controller;
 use think\Db;
--- application/config.php
+++ application/config.php
@@ -60,7 +60,7 @@ return [
     // 禁止访问模块
     'deny_module_list'       => ['common'],
     // 默认控制器名
-    'default_controller'     => 'Admin',
+    'default_controller'     => 'Index',
     // 默认操作名
     'default_action'         => 'index',
     // 默认验证器
