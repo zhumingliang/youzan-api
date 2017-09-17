1b7ec47693d3e82cbde9adcc0bb0474e55e0cfc8
mingliangZhu <mlz@email.com>
2017-09-17 16:44:20 +0800

    测试

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,13 +5,12 @@ namespace app\index\controller;
 //use youzanlib\Toekn;
 
 use think\Loader;
+use youzanlib\Toekn;
 
 class Index
 {
     public function index()
     {
-        Loader::import('youzanlib.Token');
-        $token = new \youzanlib\Toekn();
-
+       $token=new Toekn();
     }
 }
