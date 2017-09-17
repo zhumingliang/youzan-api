00a9cced1deef25de2c8c5c25692b24b08b23af3
mingliangZhu <mlz@email.com>
2017-09-17 18:29:55 +0800

    测试

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -11,15 +11,18 @@ namespace app\api\controller;
 use think\Controller;
 use think\Exception;
 use think\Loader;
+use YouZanLib\Token;
 
 class Index extends Controller
 {
     public function index()
     {
-        Loader::import('YouZanLib.YZSignClient');
-        $appId = '5a6f9470c23e160ea4'; //请填入你有赞店铺后台-营销-有赞API的AppId
-        $appSecret = '888373e35860e94098f43349ae340f57';//请填入你有赞店铺后台-营销-有赞API的AppSecret
-        $client = new \YZSignClient($appId, $appSecret);
+        /* Loader::import('YouZanLib.YZSignClient');
+         $appId = '5a6f9470c23e160ea4'; //请填入你有赞店铺后台-营销-有赞API的AppId
+         $appSecret = '888373e35860e94098f43349ae340f57';//请填入你有赞店铺后台-营销-有赞API的AppSecret
+         $client = new \YZSignClient($appId, $appSecret);*/
+
+        $token = new  Token();
     }
 
 
---
+++ extend/YouZanLib/Token.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhumingliang
+ * Date: 2017/9/17
+ * Time: 下午6:29
+ */
+
+namespace YouZanLib;
+
+
+class Token
+{
+
+
+}
