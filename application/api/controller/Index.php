f272982e2071cf2aadd70877f9f5c0dc6d57d444
mingliangZhu <mlz@email.com>
2017-09-18 13:52:53 +0800

    测试

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -37,12 +37,11 @@ class Index extends Controller
             $keys['code'] = $code;//如要刷新access_token，这里为$keys['refresh_token']
             $keys['redirect_uri'] = YouZanConfig::$REDIRECT_URL;
             $data = $token->get_token($type, $keys);
+            if (is_null($data['access_token'])) {
+                echo $data['access_token'];
+            }
 
-            echo '<pre>';
-            var_dump(
-                $data
-        );
-            echo '</pre>';
+
         }
 
     }
